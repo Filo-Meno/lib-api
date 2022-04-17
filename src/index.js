f0a2e1da1f33928e21671beb8cf267b1ac4a2990
DaeLliux99 <dino_esteban@hotmail.com>
2022-04-17 11:03:57 -0500

    cors arreglados

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ require('./models/asociaciones');
 app.set('port', process.env.PORT || 4000);
 
 // Middlewares
-app.options(cors());
+app.use(cors());
 /*app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))*/
 
